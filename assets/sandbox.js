52b873743d51cf358ac6ea91f30e70a2ef0d7c51
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-01-29 20:37:54 -0600

    render weather icon from api

--- assets/sandbox.js
+++ assets/sandbox.js
@@ -1,3 +1,7 @@
 $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Dallas&units=imperial&appid=8f9a11f5149a0f8020ecd4c8516f848a", function(data) {
     console.log(data);
+
+    var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    
+    $('.icon').attr('src', icon);
 })
